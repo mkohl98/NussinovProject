65d655fe9d16014baee81938ac1ffc36519b2406
Marcel <marcel_kohl2@gmx.de>
2019-05-13 10:01:54 +0200

    first commit, started scripting

---
+++ algorithm.py
@@ -0,0 +1,16 @@
+sub = input("Enter your sequence or fasta file.")
+
+if sub == *.fasta:
+    parser(sub)
+else:
+    sub = sequence
+
+#create valid sequence from .fasta
+def parser(x):
+    pass
+
+#define class matrix
+class matrix(x,y):
+    pass
+
+
