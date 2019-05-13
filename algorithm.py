c8ea7254227935796010f51aec68ab5ca8d3e9e3
Marcel <marcel_kohl2@gmx.de>
2019-05-13 20:36:40 +0200

    changed dictionary, keep working on filling matrix

--- algorithm.py
+++ algorithm.py
@@ -16,7 +16,7 @@ seq = "ACUGUUACCGUAAGC"
 
 m = len(seq)
 
-
+l = 4 #minimal loop lenght
 def init(m):
     matrix = list(list(0 for x in range(m)) for x in range(m))
     return(matrix)
@@ -37,38 +37,33 @@ matrix = init(m)
 #fill matrix
 
 pairs = {
-    "A,A": 0,
-    "U,U": 0,
-    "G,G": 0,
-    "C,C": 0,
-    "A,U": 1,
-    "U,A": 1,
-    "G,C": 1,
-    "C,G": 1,
-    "A,C": 0,
-    "C,A": 0,
-    "A,G": 0,
-    "G,A": 0,
-    "U,C": 0,
-    "C,U": 0,
-    "U,G": 0,
-    "G,U": 0,
+    ('A', 'A'): 0,
+    ('U', 'U'): 0,
+    ('G', 'G'): 0,
+    ('C', 'C'): 0,
+    ('A', 'U'): 1,
+    ('U', 'A'): 1,
+    ('G', 'C'): 1,
+    ('C', 'G'): 1,
+    ('A', 'C'): 0,
+    ('C', 'A'): 0,
+    ('A', 'G'): 0,
+    ('G', 'A'): 0,
+    ('U', 'C'): 0,
+    ('C', 'U'): 0,
+    ('U', 'G'): 0,
+    ('G', 'U'): 0,
 }
 
 #scoring
 
 
-
-
-#max argument
-
 for n in range(2,m):
     for j in range(n,m):
         i = j -n
-        matrix[i][j] = max(
-            matrix[i+1][j],
-            matrix[i][j-1],
-            matrix[i+1][j-1]
+        matrix[i][j] = max(matrix[i][j-1],              #unpaired
+                           matrix[i+1][j],
+                           matrix[i+1][j - 1] + pairs[seq[i],seq[j]]
         )
 
 
