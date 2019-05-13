6b350be4ee66488be7267814e139da32a5cfbeac
Marcel <marcel_kohl2@gmx.de>
2019-05-13 23:09:42 +0200

    keep working on filling matrix, added bifurkations

--- algorithm.py
+++ algorithm.py
@@ -16,7 +16,7 @@ seq = "ACUGUUACCGUAAGC"
 
 m = len(seq)
 
-l = 4 #minimal loop lenght
+minloop = 4 #minimal loop lenght
 def init(m):
     matrix = list(list(0 for x in range(m)) for x in range(m))
     return(matrix)
@@ -62,8 +62,11 @@ for n in range(2,m):
     for j in range(n,m):
         i = j -n
         matrix[i][j] = max(matrix[i][j-1],              #unpaired
-                           matrix[i+1][j],
-                           matrix[i+1][j - 1] + pairs[seq[i],seq[j]]
+                           matrix[i+1][j],              #unpaired
+                           matrix[i+1][j-1] + pairs[seq[i],seq[j]],     #paired
+                           max(
+                               [(matrix[i][k] + matrix[k+1][j] * pairs[seq[k],seq[j]]) for x in i <= k < (j-1)]     #bifurkation
+                           )
         )
 
 
