755e45b90d760b5489bbe655258ff7129bd17300
mkohl <marcel_kohl2@gmx.de>
2019-05-24 10:07:52 +0200

    init traceback

--- algorithm.py
+++ algorithm.py
@@ -1,26 +1,25 @@
-# sub = input("Enter your sequence or fasta file.")
-#
-# if sub == *.fasta:
-#     parser(sub)
-# else:
-#     sub = seq
-
-#create valid sequence from .fasta
-def parser(x):
-    pass
-    return(seq)
+# submission = input("Enter your sequence.")
 
 
 #testsequence
-#seq = "ACUGUUACCGUAAGC"
+
+seq = "ACGAGGAAUCGUA"
+
 
 m = len(seq)
 
-minloop = 4 #minimal loop lenght
+minloop = 2                #minimal loop lenght
+
+
+
+# initialise matrix
+
 def init(m):
     matrix = list(list(0 for x in range(m)) for x in range(m))
     return(matrix)
 
+
+#show matrix
 
 def show(matrix, m):
     for i in range(m):
@@ -29,12 +28,7 @@ def show(matrix, m):
         print()
 
 
-#initialise matrix
-
-matrix = init(m)
-
-
-#fill matrix
+#scoring matrix
 
 pairs = {
     ('A', 'A'): 0,
@@ -55,25 +49,64 @@ pairs = {
     ('G', 'U'): 0,
 }
 
-#scoring
+
+#filling
+
+def fill(matrix,m,pairs):
+    for n in range(2,m):
+        for j in range(n,m):
+            i = j -n
+            matrix[i][j] = max(matrix[i][j-1],              #unpaired
+                               matrix[i+1][j],              #unpaired
+                               matrix[i+1][j-1] + pairs[seq[i],seq[j]],     #paired
+                               max(
+                                   [(matrix[i][k] + matrix[k+1][j] * pairs[seq[k],seq[j]]) for k in range(i,j)]     #bifurkation
+                               )
+            )
+    return(matrix)
 
 
-for n in range(2,m):
-    for j in range(n,m):
-        i = j -n
-        matrix[i][j] = max(matrix[i][j-1],              #unpaired
-                           matrix[i+1][j],              #unpaired
-                           matrix[i+1][j-1] + pairs[seq[i],seq[j]],     #paired
-                           max(
-                               [(matrix[i][k] + matrix[k+1][j] * pairs[seq[k],seq[j]]) for k in range(i,j)]     #bifurkation
-                           )
-        )
+#traceback
+
+def traceback(matrix,m):
+    dotbracket = ['.' for x in range(m)]
+    trace = [(0,m-1)]
+    while trace is not []:
+        if i >= j:
+            pass
+        elif matrix[i+1][j] == matrix[i][j]:
+            trace.append(matrix[i+1][j])
+        elif matrix[i][j-1] == matrix[i][j]:
+            trace.append(matrix[i][j])
+        elif (matrix[i+1][j-1] + pairs[seq[i],pairs[j]]) == matrix[i][j]:
+            trace.append(matrix[i+1][j-1])
+            dotbracket[i] = '('
+            dotbracket[j] = ')'
+        else:
+            for k in range(i+1,j-1):
+                if (matrix[i][k] + matrix[k+1][j]) == matrix[i][j]:
+                    trace.append(matrix[k+1][j])
+                    trace.append(matrix[i][k])
+                else:
+                    break
+    return(dotbracket)
 
 
 
 
 
+def calculateenergy():
+    pass
 
 
 
+
+
+matrix = init(m)
+score = fill(matrix,m,pairs)
 show(matrix, m)
+
+print(score[0][m-1])
+
+traceback(matrix,m)
+print(dotbracket)
